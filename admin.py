c9684590de10bdda078838a1e3ca984218fe84ec
CHRIS SELF <chris@vitruehealth.com>
2022-10-14 21:52:45 +0100

    Install cors headers.

---
+++ admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from vocabDecks.models import Card
+
+admin.site.register(Card)
--- settings.py
+++ settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'vocabDecks',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'vocabDecks.urls'
+
+ALLOWED_HOSTS=['*'] 
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
